8525f88ae99c377281f616ef451b7dba87a4e838
Barun <kharelbarun@gmail.com>
2025-08-05 11:47:28 +0545

    improve type

--- jsx.d.ts
+++ jsx.d.ts
@@ -1,51 +1,87 @@
+import ComponentChildren from '../xylem-js/types/ComponentChildren.js';
 import Subscriber from '../xylem-js/types/Subscriber.js';
 import Supplier from '../xylem-js/types/Supplier.js';
 
-type PrimitiveAttribute = boolean|string|number|null|undefined;
+type PrimitiveAttribute = boolean|string|number|bigint|null|undefined;
 
 type Attribute = (
 	PrimitiveAttribute
 	| Supplier<PrimitiveAttribute>
 );
 
+type ComponentChild = ComponentChildren extends (infer U)[] ? U : never;
+
+type EventHandlers = {
+	[K in keyof GlobalEventHandlersEventMap as `on:${K}`]: ((ev: GlobalEventHandlersEventMap[K]) => void)
+};
+
 export namespace JSX {
 	interface IntrinsicElements {
 		[tagName: string]: (
+			Partial<EventHandlers>
+			&
 			{
 				class?:
-					Attribute
-					| { [className: string]: Supplier<boolean> }
+					| Attribute
+					| { [className: string]: boolean|Supplier<boolean> }
 					| Array<
 						string
 						| Supplier<string>
-						| { [className: string]: Supplier<boolean> }
+						| { [className: string]: boolean|Supplier<boolean> }
 					>,
 				style?:
-					Attribute
-					| { [propertyName: string] : Supplier<false|string> }
+					| Attribute
+					| { [propertyName: string]: Supplier<false|string> }
 					| Array<
 						string
 						| Supplier<string>
 						| { [propertyName: string]: Supplier<false|string> }
 					>,
-				[event: `on:${string}`]: EventListenerOrEventListenerObject,
-				'<>'?: Subscriber<HTMLElement>,
+				'<>'?: Subscriber<HTMLElement>|Subscriber<SVGElement>|Subscriber<MathMLElement>,
+				children?:
+					| string|number|bigint|Supplier<string|number|bigint>
+					| ComponentChild
+					| Array<
+						| string|number|bigint|Supplier<string|number|bigint>
+						| ComponentChild | ComponentChildren
+					>
+				,
 			}
 			&
 			{
 				// this has to be superset of previous types
 				[other: string]:
-					Attribute
-					| { [className: string]: Supplier<boolean> }
+					| Attribute
+
+					// class attribute
+					| { [className: string]: boolean|Supplier<boolean> }
+					| Array<
+						string
+						| Supplier<string>
+						| { [className: string]: boolean|Supplier<boolean> }
+					>
+
+					// style attribute
 					| { [propertyName: string]: Supplier<false|string> }
 					| Array<
 						string
 						| Supplier<string>
-						| { [className: string]: Supplier<boolean> }
 						| { [propertyName: string]: Supplier<false|string> }
 					>
-					| EventListenerOrEventListenerObject
-					| Subscriber<HTMLElement>
+
+					// event listener
+					| EventHandlers[keyof EventHandlers]
+
+					// element subscriber
+					| Subscriber<HTMLElement>|Subscriber<SVGElement>|Subscriber<MathMLElement>
+
+					// children
+					| string|number|bigint|Supplier<string|number|bigint>
+					| ComponentChild
+					| Array<
+						| string|number|bigint|Supplier<string|number|bigint>
+						| ComponentChild | ComponentChildren
+					>
 			}
 		),
 	}
